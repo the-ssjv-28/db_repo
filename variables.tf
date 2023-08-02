6b6ea2a2a88ad555ae26622ed65a69016a896f15
the-ssjv-28 <83805706+the-ssjv-28@users.noreply.github.com>
2023-08-02 14:14:49 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "repo_conf" {
+
+}
+
+variable "server_name" {
+
+}
+
+variable "resource_group_name" {
+
+}
+
+variable "charset" {
+  default = "LATIN1"
+}
+
+variable "collation" {
+  default = "en_GB.utf8"
+}
