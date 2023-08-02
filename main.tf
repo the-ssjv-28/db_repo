f5ae35f9d49815115d843f01f7c097daf8243dda
the-ssjv-28 <83805706+the-ssjv-28@users.noreply.github.com>
2023-08-02 15:19:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ for_each = var.repo_conf
   name                = each.value.name
   resource_group_name = var.resource_group_name
   server_name         = var.server_name
-  charset             = lookup(var.repo_conf[each.key], "charset", var.charset)  
+  charset             = each.value.charset
   collation           = each.value.collation
 }
